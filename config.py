57600820a42b196f7cc3c558164138b779f58f42
Jeffrey Cheung <jeffrey@treez.io>
2022-12-16 17:40:55 -0800

    configs redacted

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
 import os
 
+os.environ['CLIENT_ID'] = "[REDACTED]"
+os.environ['CLIENT_SECRET'] = "[REDACTED]"
+
+os.environ['MLR_SEARCH_TEST'] = "[REDACTED]"
+os.environ['MLR_WEBHOOK_TEST'] = "[REDACTED]"
+os.environ['MILR_SEARCH_TEST'] = "[REDACTED]"
+os.environ['MILR_WEBHOOK_TEST'] = "[REDACTED]"
+os.environ['UMP_SEARCH_TEST'] = "[REDACTED]"
+os.environ['UMP_WEBHOOK_TEST'] = "[REDACTED]"
+
 os.environ['TOKEN'] = "[REDACTED]"
+os.environ['USER_AGENT'] = "[REDACTED]"
