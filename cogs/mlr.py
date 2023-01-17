5ac51c1bb92c418bd87f2008dbf36d8b8d055dd2
Jeffrey Cheung <jeffrey@treez.io>
2023-01-17 12:24:17 -0800

    add swings command

--- cogs/mlr.py
+++ cogs/mlr.py
@@ -21,7 +21,7 @@ class MLR(commands.Cog):
 
     @commands.command()
     async def pitches(self, ctx, league, player_id, number_of_pitches=50):
-        """Returns line chart of recent pitches"""
+        """[league] [playerId] [optional:numberOfPitches]"""
         player = (requests.get("https://www.swing420.com/api/players/id/" + player_id)).json()
         response = (
             requests.get(f"https://www.swing420.com/api/plateappearances/pitching/{league}/{player_id}")).json()
@@ -40,6 +40,38 @@ class MLR(commands.Cog):
         plt.xlim(0, number_of_pitches + 1)
         plt.grid(True)
         plt.plot(list_of_numbers, list_of_pitches, marker='o', linestyle='dashed', linewidth=1, markersize=7)
+        plt.savefig('graph.png')
+        plt.close()
+
+        with open('graph.png', 'rb') as f:
+            file = io.BytesIO(f.read())
+
+        image = discord.File(file, filename='graph.png')
+
+        await ctx.send(file=image)
+        os.remove('graph.png')
+
+    @commands.command()
+    async def swings(self, ctx, league, player_id, number_of_swings=50):
+        """[league] [playerId] [optional:numberOfSwings]"""
+        player = (requests.get("https://www.swing420.com/api/players/id/" + player_id)).json()
+        response = (
+            requests.get(f"https://www.swing420.com/api/plateappearances/batting/{league}/{player_id}")).json()
+
+        list_of_swings = []
+        for x in response:
+            if (x['pitch'] != 0) & (x['swing'] != 0) & (x['session'] != 0):
+                list_of_swings.append(x['swing'])
+        list_of_swings = list_of_swings[-number_of_swings:]
+
+        number_of_swings = len(list_of_swings)
+        list_of_numbers = list(range(1, number_of_swings + 1))
+
+        plt.title(player['playerName'] + ' last ' + str(number_of_swings) + " swings in " + str(league))
+        plt.ylim(0, 1000)
+        plt.xlim(0, number_of_swings + 1)
+        plt.grid(True)
+        plt.plot(list_of_numbers, list_of_swings, marker='o', linestyle='dashed', linewidth=1, markersize=7)
         plt.savefig('graph.png')
         plt.close()
 
