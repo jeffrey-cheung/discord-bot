03cdd8d4a0cbafc56aea2d57adc3b651bf0af5c9
Jeffrey Cheung <jeffrey@treez.io>
2023-02-03 16:37:17 -0800

    Add !player

--- cogs/mlr.py
+++ cogs/mlr.py
@@ -1,12 +1,14 @@
 import constants
 import discord
 import io
+import json
 import matplotlib.pyplot as plt
 import os
 import random as rdm
 import requests
 from discord.ext import commands
 
+colors = json.loads(constants.MLR_COLORS)
 HYPE_LIST = constants.HYPE_LIST
 
 
@@ -82,6 +84,48 @@ class MLR(commands.Cog):
 
         await ctx.send(file=image)
         os.remove('graph.png')
+
+    @commands.command()
+    async def player(self, ctx, *, name):
+        """[name]"""
+        playerList = (requests.get("https://www.swing420.com/api/players")).json()
+        matchList = []
+        tooManyResults = "Your search for " + name + " returned too many results"
+
+        for i in playerList:
+            if name.replace(" ", "").lower() in i['playerName'].replace(" ", "").lower():
+                matchList.append(i)
+                tooManyResults += "\n" + i['playerName']
+
+        if len(matchList) == 1:
+            player = matchList[0]
+            team_abbreviation = player['Team']
+            position = player['priPos']
+            if player['secPos'] != "":
+                position += "/" + player['secPos']
+            if player['tertPos'] != "":
+                position += "/" + player['tertPos']
+
+            team_color = int(colors.get(team_abbreviation, "DEFAULT"), 16)
+            embed = discord.Embed(color=team_color)
+
+            embed.set_author(name=player['playerName'])
+
+            embed.add_field(name="Team", value=team_abbreviation, inline=True)
+            embed.add_field(name="Hand", value=player['hand'], inline=True)
+            embed.add_field(name="Position", value=position, inline=True)
+            embed.add_field(name="Batting Type", value=player['batType'], inline=True)
+            embed.add_field(name="Pitching Type", value=player['pitchType'], inline=True)
+            embed.add_field(name="", value="", inline=True)
+            embed.add_field(name="Discord", value=player['discordName'], inline=True)
+            embed.add_field(name="Reddit", value=player['redditName'], inline=True)
+            embed.add_field(name="Player ID", value=player['playerID'], inline=True)
+
+            await ctx.send(embed=embed)
+        elif len(matchList) == 0:
+            await ctx.send("No results found for " + name)
+        else:
+            await ctx.send(tooManyResults)
 
     @commands.command()
     async def hype(self, ctx):
