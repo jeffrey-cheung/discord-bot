2c14d8e5cceeb9ef84cddd006fa389eb2159e39d
Jeffrey Cheung <jeffrey@treez.io>
2023-03-10 14:51:19 -0800

    ump2 to configs

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ os.environ['MILR_SEARCH_TEST'] = "[REDACTED]"
 os.environ['MILR_WEBHOOK_TEST'] = "[REDACTED]"
 os.environ['UMP_SEARCH_TEST'] = "[REDACTED]"
 os.environ['UMP_WEBHOOK_TEST'] = "[REDACTED]"
+os.environ['UMP2_SEARCH_TEST'] = "[REDACTED]"
+os.environ['UMP2_WEBHOOK_TEST'] = "[REDACTED]"
 
 os.environ['TOKEN'] = "[REDACTED]"
 os.environ['USER_AGENT'] = "[REDACTED]"
