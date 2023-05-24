b7290fb6a36e6b58c7d5f284d3ef9b9d86ade4f5
Jeffrey Cheung <jeffrey@treez.io>
2023-05-24 00:49:38 -0700

    refactor

--- cogs_shadowballbot/shadowball.py
+++ cogs_shadowballbot/shadowball.py
@@ -6,6 +6,7 @@ game_started = False
 current_guesses = {}
 current_score = {}
 usernames = {}
+waiting_for_pitch = False
 
 
 def calculate_diff(swing, pitch):
@@ -16,8 +17,16 @@ def calculate_diff(swing, pitch):
 
 
 def calculate_score(diff):
-    if diff <= 100:
-        return 5
+    if diff <= 5:
+        return 10
+    elif diff <= 30:
+        return 8
+    elif diff <= 60:
+        return 6
+    elif diff <= 90:
+        return 4
+    elif diff <= 150:
+        return 2
     elif diff <= 200:
         return 1
     elif diff <= 300:
@@ -26,36 +35,50 @@ def calculate_score(diff):
         return -1
 
 
-def reset_guesses():
-    global current_guesses
-    current_guesses = {}
-
-
 def reset_game():
     global game_started
     global current_guesses
     global current_score
     global usernames
+    global waiting_for_pitch
     game_started = False
     current_guesses = {}
     current_score = {}
     usernames = {}
+    waiting_for_pitch = False
 
 
 def display_guess_results(pitch):
+    if not current_guesses:
+        return "No one guessed"
     string = ""
     for user in current_guesses:
         diff = calculate_diff(current_guesses[user], pitch)
         score = calculate_score(diff)
-        string += f"```{usernames[user]} guessed {current_guesses[user]}, for a diff of {diff}, scoring {score} points.```\n"
+        string += f"**{usernames[user]}** guessed **{current_guesses[user]}**, for a diff of **{diff}**, scoring **{score}** points."
     return string
 
 
 def display_scoreboard():
-    string = ""
+    string = "**Scoreboard**:"
     for user in current_score:
-        string += f"```{usernames[user]}: {current_score[user]}```\n"
+        string += f"```{usernames[user]}: {current_score[user]}```"
     return string
+
+
+def result_pitch(ctx, pitch):
+    global current_guesses
+    global waiting_for_pitch
+    waiting_for_pitch = False
+    for user in current_guesses:
+        diff = calculate_diff(current_guesses[user], pitch)
+        score = calculate_score(diff)
+        if user not in current_score:
+            current_score.update({user: score})
+        else:
+            current_score.update({user: current_score[user] + score})
+    await ctx.send(f"Pitch was **{pitch}**.\n\n{display_guess_results(pitch)}\n\n{display_scoreboard()}")
+    current_guesses = {}
 
 
 class SHADOWBALL(commands.Cog):
@@ -66,6 +89,7 @@ class SHADOWBALL(commands.Cog):
     async def start_game(self, ctx):
         """Starts a game of Shadow Ball"""
         global game_started
+        global waiting_for_pitch
         if game_started is True:
             await ctx.send(f"A game is currently in progress")
             return
@@ -77,9 +101,7 @@ class SHADOWBALL(commands.Cog):
         )
 
         await ctx.send(f"New game started")
-
         game_started = True
-        waiting_for_pitch = False
 
         # submission = await reddit.submission("13fm4bi")
         # comments = await submission.comments()
@@ -104,17 +126,14 @@ class SHADOWBALL(commands.Cog):
                 third_line = lines[2].lstrip()
                 team_abbreviation = third_line[0:3]
 
-            if waiting_for_pitch is False and comment.parent_id[
-                                              0:3] != "t1_" and comment.author == "FakeBaseball_Umpire" and len(
-                lines) == 3:
+            if waiting_for_pitch is False and comment.parent_id[0:3] != "t1_" and comment.author == "FakeBaseball_Umpire" and len(lines) == 3:
                 waiting_for_pitch = True
                 await ctx.send(f"<@&1060698924584804416> AB Posted!```{comment.body}```")
             elif waiting_for_pitch is True and comment.author == "FakeBaseball_Umpire" and len(lines) >= 5:
                 fifth_to_last_line = lines[len(lines) - 5].lstrip()
                 if fifth_to_last_line[0:6] == "Pitch:":
                     pitch = fifth_to_last_line.split(" ")[1]
-                    waiting_for_pitch = False
-                    await ctx.send(f"Pitch was {pitch}")
+                    result_pitch(ctx, pitch)
 
     @commands.command()
     async def end_game(self, ctx):
@@ -138,28 +157,17 @@ class SHADOWBALL(commands.Cog):
 
         current_guesses.update({ctx.message.author.id: guess})
         usernames.update({ctx.message.author.id: ctx.message.author.name})
-        await ctx.send(f"Received your guess of {guess}")
+        await ctx.send(f"Received your guess of **{guess}**")
 
     @commands.command()
     async def pitch(self, ctx, pitch):
         """Submit pitch manually"""
         global game_started
-        global current_guesses
-        global current_score
         if game_started is False:
             await ctx.send(f"There is no game in progress")
             return
 
-        for user in current_guesses:
-            diff = calculate_diff(current_guesses[user], pitch)
-            score = calculate_score(diff)
-            if user not in current_score:
-                current_score.update({user: score})
-            else:
-                current_score.update({user: current_score[user] + score})
-
-        await ctx.send(f"Pitch was {pitch}.\n{display_guess_results(pitch)}\n{display_scoreboard()}")
-        reset_guesses()
+        result_pitch(ctx, pitch)
 
 
 async def setup(bot):
