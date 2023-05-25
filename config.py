4cdefdd1bf43c7fc4383facc1e543294b66f0431
Jeffrey Cheung <jeffrey@treez.io>
2023-05-25 11:42:23 -0700

    config update

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
 # Reddit Credentials
-os.environ['CLIENT_ID'] = "[REDACTED]"
-os.environ['CLIENT_SECRET'] = "[REDACTED]"
-os.environ['USER_AGENT_PLAY_BY_PLAY'] = "[REDACTED]"
-os.environ['USER_AGENT_SHADOW_BALL'] = "[REDACTED]"
+os.environ['CLIENT_ID'] = ""
+os.environ['CLIENT_SECRET'] = ""
+os.environ['USER_AGENT_PLAY_BY_PLAY'] = ""
+os.environ['USER_AGENT_SHADOWBALLBOT'] = ""
 
 # play_by_play Credentials (Test)
-os.environ['MLR_SEARCH_TEST'] = "[REDACTED]"
-os.environ['MLR_WEBHOOK_TEST'] = "[REDACTED]"
-os.environ['MILR_SEARCH_TEST'] = "[REDACTED]"
-os.environ['MILR_WEBHOOK_TEST'] = "[REDACTED]"
+os.environ['MLR_SEARCH_TEST'] = ""
+os.environ['MLR_WEBHOOK_TEST'] = ""
+os.environ['MILR_SEARCH_TEST'] = ""
+os.environ['MILR_WEBHOOK_TEST'] = ""
 
 # play_by_play Credentials (Prod)
 os.environ['MLR_SEARCH'] = ""
@@ -19,5 +19,5 @@ os.environ['MILR_SEARCH'] = ""
 os.environ['MILR_WEBHOOK'] = ""
 
 # Discord Bot Credentials
-os.environ['TOKEN_LUDABOT'] = "[REDACTED]"
-os.environ['TOKEN_SHADOWBALL'] = "[REDACTED]"
+os.environ['TOKEN_LUDABOT'] = ""
+os.environ['TOKEN_SHADOWBALLBOT'] = ""
