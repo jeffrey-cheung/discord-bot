6db22bf18344cdb3a88088e136380a1503fd2c43
Jeffrey Cheung <jeffrey@treez.io>
2023-06-22 08:28:05 -0700

    Update fcfb.py

--- fcfb.py
+++ fcfb.py
@@ -29,6 +29,8 @@ for submission_id in submission_list:
             offense = "0"
             defense = "0"
             difference = "0"
+            down = "0"
+            play = "0"
             for y in lines:
                 if len(y) > 9 and y.split(" ")[0] == "Offense:" and y.split(" ")[1].isdigit():
                     offense = y.split(" ")[1]
@@ -37,9 +39,34 @@ for submission_id in submission_list:
                 elif len(y) > 12 and y.split(" ")[0] == "Difference:" and y.split(" ")[1].isdigit():
                     difference = y.split(" ")[1]
             if offense != "0" and defense != "0":
+                user_comment = x.parent().body.splitlines()
                 parent_comment = x
                 while parent_comment.parent_id[0:3] == "t1_":
                     parent_comment = parent_comment.parent()
                 parent_first = parent_comment.body.splitlines()
                 if "has submitted their number." in parent_first[0] and "you're up." in parent_first[0]:
-                    print(f"{x.link_id},{parent_first[0].split(' ')[0]},{offense},{defense},{difference}")
+                    if "It's 1st and " in parent_first[2]:
+                        down = "1st"
+                    elif "It's 2nd and " in parent_first[2]:
+                        down = "2nd"
+                    elif "It's 3rd and " in parent_first[2]:
+                        down = "3rd"
+                    elif "It's 4th and " in parent_first[2]:
+                        down = "4th"
+                    elif " is kicking off." in parent_first[2]:
+                        down = "kickoff"
+                    elif " just scored." in parent_first[2]:
+                        down = "pat"
+                    if "field goal " in user_comment[0].lower():
+                        play = "field goal"
+                    elif "punt " in user_comment[0].lower():
+                        play = "punt"
+                    elif "pass " in user_comment[0].lower():
+                        play = "pass"
+                    elif "run " in user_comment[0].lower():
+                        play = "run"
+                    elif "pat " in user_comment[0].lower():
+                        play = "pat"
+                    elif down == "kickoff":
+                        play = "kickoff"
+                    print(f"{x.link_id},{parent_first[0].split(' ')[0]},{offense},{defense},{difference},{down},{play}")
