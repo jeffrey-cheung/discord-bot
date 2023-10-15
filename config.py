66b966cc2fd021d4bae073230e52dbcef375778c
Jeffrey Cheung <jeffrey@treez.io>
2023-10-14 20:52:56 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,32 @@
 import os
 
 # Reddit Credentials
-os.environ['CLIENT_ID'] = ""
-os.environ['CLIENT_SECRET'] = ""
-os.environ['USER_AGENT_FCFB'] = ""
-os.environ['USER_AGENT_PLAY_BY_PLAY'] = ""
-os.environ['USER_AGENT_SHADOWBALLBOT'] = ""
+os.environ['CLIENT_ID'] = "2ELD5IWJFclyRv6vpa0NqA"
+os.environ['CLIENT_SECRET'] = "UyVX10AVwTCzIwqgXggmygf5RaGt0A"
+os.environ['USER_AGENT_FCFB'] = "eWeapons:fcfb:v1.0.0"
+os.environ['USER_AGENT_PLAY_BY_PLAY'] = "eWeapons:play_by_play:v1.0.0"
+os.environ['USER_AGENT_SHADOWBALLBOT'] = "eWeapons:shadow_ball:v1.0.0"
 
 # play_by_play Credentials (Test)
-os.environ['MLR_SEARCH_TEST'] = ""
-os.environ['MLR_WEBHOOK_TEST'] = ""
-os.environ['MILR_SEARCH_TEST'] = ""
-os.environ['MILR_WEBHOOK_TEST'] = ""
-os.environ['FCFB_SEARCH_TEST'] = ""
-os.environ['FCFB_WEBHOOK_TEST'] = ""
-os.environ['WC_SEARCH_TEST'] = ""
-os.environ['WC_WEBHOOK_TEST'] = ""
+os.environ['MLR_SEARCH_TEST'] = "Milwaukee Brewers"
+os.environ['MLR_WEBHOOK_TEST'] = "https://discord.com/api/webhooks/1088905228167938148/mEbEolBSyzJynu_3KtZ-HWLzHxvpL55rx3lz1Gp6g6Hxx2hUFEnJhbZyvVFh1k5hUvq0"
+os.environ['MILR_SEARCH_TEST'] = "Santa Rosa Scuba Divers"
+os.environ['MILR_WEBHOOK_TEST'] = "https://discord.com/api/webhooks/1088905238934728795/CCZWdQNzZUvXc1u-agxydQpJas9AsyWn05Rdwfn70SHr5dr58vAhNKGSn8ZeXK1zgdNA"
+os.environ['FCFB_SEARCH_TEST'] = "California"
+os.environ['FCFB_WEBHOOK_TEST'] = "https://discord.com/api/webhooks/1118570475354857563/2yqr2eb2Gvy9jtC9FVGynklCyPCwQ5QDi1Hdjh9X-Uvsf2ZXHSozAOwwb7NX9bQqVQ1y"
+os.environ['WC_SEARCH_TEST'] = "Vietnam"
+os.environ['WC_WEBHOOK_TEST'] = "https://discord.com/api/webhooks/1157103534966321223/U-EFsmfP437jSSvJJKt4HTkzXENOSCgAikicuInVIJOYyMQgE0ibdJT8qhRhbSf83FFI"
 
 # play_by_play Credentials (Prod)
-os.environ['MLR_SEARCH'] = ""
-os.environ['MLR_WEBHOOK'] = ""
+os.environ['MLR_SEARCH'] = "Milwaukee Brewers"
+os.environ['MLR_WEBHOOK'] = "https://discord.com/api/webhooks/898697239290056734/0AQdKiMTzMJtPFwvyJzULqWqK9kBWk7IYR5WRjc3-5kRITJ9ofNSPp0SXUXm9cQ1tLvY"
 os.environ['MILR_SEARCH'] = ""
 os.environ['MILR_WEBHOOK'] = ""
 
 # shadowball Credentials
-os.environ['ROLE_ID'] = ""
-os.environ['TEAM_ABBREV'] = ""
+os.environ['ROLE_ID'] = "1035382395706298388"
+os.environ['TEAM_ABBREV'] = "MIL"
 
 # Discord Bot Credentials
-os.environ['TOKEN_LUDABOT'] = ""
-os.environ['TOKEN_SHADOWBALLBOT'] = ""
+os.environ['TOKEN_LUDABOT'] = "MTA1MDY4Njc5Nzk5MjE3MzY4MA.GJgOX8.o6C1-UCY78H062iYIYaHlb0AkrZRPj1TXbup9w"
+os.environ['TOKEN_SHADOWBALLBOT'] = "MTExMDQ1MDI4NTAyMjgyMjQxMA.Gah1Wp.tS09DAaclEUao1mogvJt0muXCIm52LxDB4AzIk"
