a9f18f62d175302f505e9cbbdcd42120cd72460a
jeffcheung <jeffrey@treez.io>
2024-10-25 16:10:38 -0700

    fix command prefix

--- mlr_scout.py
+++ mlr_scout.py
@@ -15,7 +15,7 @@ pytz_pst = pytz.timezone('America/Los_Angeles')
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!!!", case_insensitive=True, help_command=commands.DefaultHelpCommand(),
+bot = commands.Bot(command_prefix="!", case_insensitive=True, help_command=commands.DefaultHelpCommand(),
                       intents=intents)
 
 
